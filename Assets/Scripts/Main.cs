1c69944e2e7d6277a2e4a44a0d6ed2dc8c7c4bc2
daixxtech <daixxtech@163.com>
2020-12-29 23:29:14 +0800

    [added] version 1.0;

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    private void Update() {
+        // 读取鼠标滚轮输入，修改 timeScale，主要用于观测人物状态
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Math.Abs(scroll) > 1e-5) {
+            Time.timeScale = Mathf.Clamp(Time.timeScale + scroll, 0.1F, 2);
+        }
+    }
+
+    private void OnGUI() {
+        // 显示当前 timeScale
+        GUI.Label(new Rect(0, 0, 200, 100), $"timeScale: {Time.timeScale.ToString("F2")}", new GUIStyle {fontSize = 32});
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# unity_character-controller-2d
+
+一个简单的 2D 人物控制器，可以模拟移动和多段跳。（Unity 版本 2019.4.11f1）
+
+## 操作
+
+W, A, S, D 控制角色移动；
+
+Space 跳跃（默认状态下是二段跳）；
+
+鼠标滚轮调整 timeScale；
